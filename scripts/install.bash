5767c600b08d330df044a10fddf8ff2ae15a127a
David Maier <david.maier@couchbase.com>
2016-08-15 15:33:44 +0200

    Downloading deb to temp dir

--- scripts/install.bash
+++ scripts/install.bash
@@ -4,5 +4,6 @@
 echo Installing demo ...
 
 export WORKDIR=$HOME
+export TMP=$HOME/tmp
 export PKG_DEB=couchbase-sync-gateway-enterprise_1.3.0-274_x86_64.deb
-sudo dpkg -i $WORKDIR/$PKG_DEB
+sudo dpkg -i $TMP/$PKG_DEB
