a2a3bf8f0e7b985b2242b420b8e55aae79b2c452
David Maier <david.maier@couchbase.com>
2016-08-15 15:30:56 +0200

    Downloading deb to temp dir

--- scripts/pre-install.bash
+++ scripts/pre-install.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-## Add all pre-installation steps here!
 echo Preparing setup of the demo ...
 
 export WORKDIR=$HOME
 export TMP=$HOME/tmp
-export PKG_REPO=http://packages.couchbase.com/releases
-export PKG_DEB=couchbase-sync-gateway/1.3.0/couchbase-sync-gateway-enterprise_1.3.0-274_x86_64.deb
+export PKG_REPO=http://packages.couchbase.com/releases/couchbase-sync-gateway/1.3.0
+export PKG_DEB=couchbase-sync-gateway-enterprise_1.3.0-274_x86_64.deb
 
 echo Downloading SyncGW ...
 mkdir -p $TMP
-wget $PKG_REPO/$PKG_DEB -P $WORKDIR /
+wget $PKG_REPO/$PKG_DEB > $TMP/$PKG_DEB
