97834460dc35d679294ab1f0a137504f662a14b6
David Maier <david.maier@couchbase.com>
2016-08-15 15:16:52 +0200

    Added SyncGW setup

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,3 +2,7 @@
 
 ## Add all installation steps for your demo here!
 echo Installing demo ...
+
+export WORKDIR=$HOME
+export PKG_DEB=couchbase-sync-gateway-enterprise_1.3.0-274_x86_64.deb
+sudo dpkg -i $WORKDIR/$PKG_DEB
--- scripts/pre-install.bash
+++ scripts/pre-install.bash
@@ -2,3 +2,12 @@
 
 ## Add all pre-installation steps here!
 echo Preparing setup of the demo ...
+
+export WORKDIR=$HOME
+export TMP=$HOME/tmp
+export PKG_REPO=http://packages.couchbase.com/releases
+export PKG_DEB=couchbase-sync-gateway/1.3.0/couchbase-sync-gateway-enterprise_1.3.0-274_x86_64.deb
+
+echo Downloading SyncGW ...
+mkdir -p $TMP
+wget $PKG_REPO/$PKG_DEB -P $WORKDIR /
